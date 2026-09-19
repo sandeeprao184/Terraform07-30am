d879d0c1f9f1cf16e10ace68e68500b72313e268
sandeep <sandeepkotte30@gmail.com>
2026-09-19 21:33:44 +0530

    day-7

---
+++ day-7-root-child-module/modules/compute/variables.tf
@@ -0,0 +1,9 @@
+variable "ami_id" {
+    type = string
+    description = "The AMI ID for the EC2 instance"
+}
+
+variable "instance_type" {
+    type = string
+    description = "The instance type for the EC2 instance"
+}
